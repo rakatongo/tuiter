a567918c6f39c0bebb39562c74df7bb692528bbc
Rakunho <rakatongo@hotmail.com>
2012-06-26 23:49:11 -0300

    Finish sing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by_email(params[:session][:email])
-		if user && user.authenticate(params[:sesion][:password])
+		if user && user.authenticate(params[:session][:password])
 			sign_in user
 			redirect_to user
 		else
