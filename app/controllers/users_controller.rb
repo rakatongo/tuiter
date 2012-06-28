837b849480171db332ec869c169362d40eb6eee6
Rakunho <rakatongo@hotmail.com>
2012-06-28 04:56:46 -0300

    Add users microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   end
   def show
   	@user= User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   	def create
   		@user = User.new(params[:user])
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   	before_save {|user| user.email = email.downcase }
   	before_save :create_remember_token
@@ -23,10 +24,16 @@ class User < ActiveRecord::Base
   	validates :password, presence: true, length: { minimum: 6 }
   	validates :password_confirmation, presence: true
 
+    def feed
+        #preliminary
+        Micropost.where("user_id = ?",id )
+      end
 
   	private 
 
   		def create_remember_token
   			self.remember_token = SecureRandom.urlsafe_base64  		
   		end
+
+
 end
